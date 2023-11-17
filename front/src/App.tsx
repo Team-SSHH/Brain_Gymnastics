29d801e7650ff99a67d908e77b1814dfe4e6236a
윤자현 <bajh323@naver.com>
2023-11-17 16:53:36 +0900

    Feat: 두뇌체조 프론트

---
+++ front/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import logo from "./assets/logo.png";
+
+function App() {
+  return (
+    <div className="App bg-primary">
+      <header className="App-header">
+        <img src={logo} alt="두뇌체조로고" className="w-32" />
+      </header>
+      <div>여기여기</div>
+    </div>
+  );
+}
+
+export default App;
