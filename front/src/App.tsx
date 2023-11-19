2cdfe45550b6dd5f4a8f9272fe10d4376f4ab7b0
donghyun <kdhdh001@naver.com>
2023-11-19 23:03:30 +0900

    feat: api  연결 확인

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 import logo from "./assets/logo.png";
 
 function App() {
+  useEffect(() => {
+    // axios를 사용하여 백엔드 API 호출
+    const fetchData = async () => {
+      try {
+        const response = await axios.post(
+          "http://127.0.0.1:5000/recommend/my",
+          {
+            // 요청에 필요한 데이터를 여기에 추가
+          }
+        );
+        const data = response.data;
+        // API 응답 데이터 처리
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App bg-primary">
       <header className="App-header">
