1765e2054a2c49a063ecf5a70c734f7f05aefc99
윤자현 <bajh323@naver.com>
2023-11-21 15:19:27 +0900

    Fix: tailwind 적용 안되는 문제 해결

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -8,27 +8,6 @@ import ProfilePage from "./pages/ProfilePage";
 import QuizPage from "./pages/QuizPage";
 
 function App() {
-  useEffect(() => {
-    // axios를 사용하여 백엔드 API 호출
-    const fetchData = async () => {
-      try {
-        const response = await axios.post(
-          "http://127.0.0.1:5000/recommend/my",
-          {
-            // 요청에 필요한 데이터를 여기에 추가
-          }
-        );
-        const data = response.data;
-        // API 응답 데이터 처리
-        console.log(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <BrowserRouter>
       <div className="bg-primary h-screen font-default">
