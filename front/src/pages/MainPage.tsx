aa4dc1fcf17fe8f08a005e091e508b4a921c2b6b
donghyun <kdhdh001@naver.com>
2023-11-21 22:39:50 +0900

    feat: 메인페이지 프로필로 이동하기 추가

--- front/src/pages/MainPage.tsx
+++ front/src/pages/MainPage.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import main1 from "../assets/main1.png";
 import main2 from "../assets/main2.png";
 import main3 from "../assets/main3.png";
+import main4 from "../assets/main4.png";
 
 function MainPage() {
   return (
@@ -10,13 +11,23 @@ function MainPage() {
       <p className="font-extrabold text-center text-6xl mb-4">
         김동현님 반갑습니다.
       </p>
+      <Link
+        to="/profile"
+        className="absolute top-10 right-10 w-1/5 flex flex-col items-center justify-center hover:scale-110"
+      >
+        <img src={main4} alt="내 정보" className="w-1/5 no-drag" />
+        <p className="text-2xl font-bold text-center pt-8">내 정보 확인하기</p>
+      </Link>
       <div className="bg-secondary flex items-center justify-center w-10/12 rounded-3xl p-5 h-2/3">
-        <div className="w-1/3 flex flex-col items-center justify-between h-2/3  hover:scale-110">
+        <Link
+          to="/readNews"
+          className="w-1/3 flex flex-col items-center justify-between h-2/3  hover:scale-110"
+        >
           <img src={main1} alt="뉴스 듣기" className="w-3/5 no-drag" />
           <p className="text-4xl font-bold text-center pt-8">
             오늘의 뉴스 듣기
           </p>
-        </div>
+        </Link>
 
         <Link
           to="/readNews"
